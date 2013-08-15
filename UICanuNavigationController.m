b9922f77ac2dc85e29bb5ae18daf0d7c317b3c01
Rcalaf <rcalaf.cot@gmail.com>
2013-08-15 22:05:22 +0200

    improved nav control

--- UICanuNavigationController.m
+++ UICanuNavigationController.m
@@ -11,7 +11,8 @@
 #import "NewActivityViewController.h"
 #import "Activity.h"
 
-@interface UICanuNavigationController ()
+
+@interface UICanuNavigationController () <UIGestureRecognizerDelegate>
     
 @end
 
@@ -24,7 +25,7 @@
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         self.navigationBarHidden = YES;
-        self.control = [[UIView alloc] initWithFrame:CGRectMake(-3.0, 423.0, 63.0, 63.0)];
+        self.control = [[UIView alloc] initWithFrame:CGRectMake(-3.0, 420.0, 63.0, 63.0)];
         self.control.backgroundColor = [[UIColor alloc] initWithPatternImage:[UIImage imageNamed:@"navmenu_world.png"]];
         [self.view addSubview:self.control];
     }
@@ -33,7 +34,7 @@
 
 - (IBAction)goProfile:(UISwipeGestureRecognizer *)gesture{
     [UIView animateWithDuration:0.3 animations:^{
-        _control.frame = CGRectMake(260.0, 423.0, 63.0, 63.0);
+        _control.frame = CGRectMake(260.0, 420.0, 63.0, 63.0);
     }];
     //NSLog(@"Load Profile view");
     self.control.backgroundColor = [[UIColor alloc] initWithPatternImage:[UIImage imageNamed:@"menunav_me.png"]];
@@ -43,22 +44,47 @@
 
 }
 
--(IBAction)createActivity:(UITapGestureRecognizer *)gesture{
-    NewActivityViewController *nac = [[NewActivityViewController alloc] init];
-    [self presentViewController:nac animated:YES completion:nil];
-}
-/*
-- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event{
-    //_control.frame = CGRectMake(_control.frame.origin.x, 0.0, _control.frame.size.width, _control.frame.size.height);
-    
-    NSLog(@"%@",touches);
+-(IBAction)bounce:(UITapGestureRecognizer *)gesture{
+    NSLog(@"Bounce");
    
-}*/
+    [UIView animateWithDuration:.2 animations:^{
+        CGRect frame = _control.frame;
+        frame.origin.y = 400.0f;
+        _control.frame = frame;
+    } completion:^(BOOL finished){
+        [UIView animateWithDuration:.1 animations:^{
+            CGRect frame = _control.frame;
+            frame.origin.y = 420.0f;
+            _control.frame = frame;
+        }];
+    }];
+}
+
+-(IBAction)testSwipeUp:(UIPanGestureRecognizer *)recognizer
+{
+  //  NSLog(@"test action:%@",recognizer);
+    CGPoint location = [recognizer locationInView:self.view];
+  //  NSLog(@"location x:%f",location.x);
+    if (location.y < 423.0f && location.y > 0.0f) {
+        _control.frame = CGRectMake(_control.frame.origin.x, location.y, _control.frame.size.width, _control.frame.size.height);
+    }
+    
+    if (([recognizer state] == UIGestureRecognizerStateEnded) || ([recognizer state] == UIGestureRecognizerStateCancelled)) {
+        if (_control.frame.origin.y < 318.0f) {
+            NewActivityViewController *nac = [[NewActivityViewController alloc] init];
+            [self presentViewController:nac animated:YES completion:nil];
+        }
+        [UIView animateWithDuration:0.3 animations:^{
+            _control.frame = CGRectMake(_control.frame.origin.x, 420.0,_control.frame.size.width, _control.frame.size.height);
+        }];
+    }
+}
+
 
 
 - (IBAction)goActivities:(UISwipeGestureRecognizer *)gesture{
     [UIView animateWithDuration:0.3 animations:^{
-        _control.frame = CGRectMake(-3.0, 423.0, 63.0, 63.0);
+        _control.frame = CGRectMake(-3.0, 420.0, 63.0, 63.0);
         
     }];
    // NSLog(@"Load Activities view");
@@ -78,20 +104,34 @@
     UISwipeGestureRecognizer *goActivitiesGesture = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(goActivities:)];
     goActivitiesGesture.direction = UISwipeGestureRecognizerDirectionLeft;
     
-   /* UISwipeGestureRecognizer *createActivityGesture = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(createActivity:)];
-    createActivityGesture.direction = UISwipeGestureRecognizerDirectionUp;*/
     
-    UITapGestureRecognizer *createActivityGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(createActivity:)];
+    UIPanGestureRecognizer *upGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(testSwipeUp:)];
+    //swipeGesture.direction = UISwipeGestureRecognizerDirectionUp;
+    upGesture.delegate = self;
+    
+    UIPanGestureRecognizer *swipeGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(test2SwipeUp:)];
+    swipeGesture.delegate = self;
+    
+    UITapGestureRecognizer *bounceGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(bounce:)];
     
     [_control addGestureRecognizer:goProfileGesture];
     [_control addGestureRecognizer:goActivitiesGesture];
-    [_control addGestureRecognizer:createActivityGesture];
+    [_control addGestureRecognizer:bounceGesture];
+  //  [_control addGestureRecognizer:swipeGesture];
+    [_control addGestureRecognizer:upGesture];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer
+{
+   // NSLog(@"one: %@",gestureRecognizer);
+   // NSLog(@"other: %@",otherGestureRecognizer);
+    return YES;
 }
 
 @end
